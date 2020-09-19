39d409d8c0a32492461087449d59ab8446f46c2f
Vit <s1te@bk.ru>
2020-09-19 17:42:23 +0300

    add title and description to site configs

--- app/view/main.html.php
+++ app/view/main.html.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="<?=$configs['description']?>">
 	<?php include_once $_SERVER['DOCUMENT_ROOT'] . '/includes/meta.php'; ?>
 
     <!-- Bootstrap CSS -->
@@ -14,7 +15,7 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
  
-    <title><?=$configs['heading1']?></title>
+    <title><?=$configs['title']?></title>
   </head>
   
 <body>
